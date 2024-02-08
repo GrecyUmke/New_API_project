ec1b1843e76d568258d16cd2480e250787114dd7
GrecyUmke@github.com <grecyumke21@navgurukul.org>
2024-02-08 19:00:34 +0530

    All inlie css remove and give the css

--- app/Headlines.js
+++ app/Headlines.js
@@ -4,16 +4,19 @@ import "bootstrap/dist/css/bootstrap.css";
 const Headlines = () =>{
     return(
         <div className="container">
-            <div className="row newsline">
-                <div className="col-md-4 mt-4 border border-dark headlinebox"></div>  
-                <div className="col-md-4 mt-4 border border-dark headlinebox"></div>                 
-                <div className="col-md-4 mt-4 border border-dark headlinebox"></div> 
-                <div className="col-md-4 mt-4 border border-dark headlinebox"></div>  
-                <div className="col-md-4 mt-4 border border-dark headlinebox"></div>                 
-                <div className="col-md-4 mt-4 border border-dark headlinebox"></div> 
-                <div className="col-md-4 mt-4 border border-dark headlinebox"></div>  
-                <div className="col-md-4 mt-4 border border-dark headlinebox"></div>                 
-                <div className="col-md-4 mt-4 border border-dark headlinebox"></div> 
+            <div className="text-danger headtext">
+                <span>TOP NEWS FROM INDIA</span>
+            </div>
+            <div className="row">
+                <div className="col-md-4 border border-dark headlinebox"></div>  
+                <div className="col-md-4 border border-dark headlinebox"></div>                 
+                <div className="col-md-4 border border-dark headlinebox"></div> 
+                <div className="col-md-4 border border-dark headlinebox"></div>  
+                <div className="col-md-4 border border-dark headlinebox"></div>                 
+                <div className="col-md-4 border border-dark headlinebox"></div> 
+                <div className="col-md-4 border border-dark headlinebox"></div>  
+                <div className="col-md-4 border border-dark headlinebox"></div>                 
+                <div className="col-md-4 border border-dark headlinebox"></div> 
             </div>
         </div>
     )
--- app/Search.js
+++ app/Search.js
@@ -5,11 +5,10 @@ import "bootstrap/dist/css/bootstrap.css";
 const Search = () =>{
     return(
         <div className="container">
-            <div className="row align-item-center">
-                <div className="text-center mt-4">
-                    <input type="text" value="Search" id="search" style={{height:"60px", width:"303px" }}/>
-                    <input type="submit" value="Go" id="button" style={{height:"61px", width:"61px" ,background:"#E84848"}}/>
-                    <div id="box"></div>
+            <div className="row">
+                <div className="text-center">
+                    <input type="text" value="Search" id="search" className="searchbox"/>
+                    <input type="submit" value="Go" id="button" className="gobutton"/>
                 </div>
             </div>
         </div>
